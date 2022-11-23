15cee7890079b26afa3c068337ab45b64848a117
Adrian Wójcik <wojcik.adrian@protonmail.com>
2022-11-23 19:26:02 +0100

    First migrate

--- Django/my_site/my_app/views.py
+++ Django/my_site/my_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def example_view(request):
+    # my-app/templates/my_app/example.html
+    return render(request, 'my_app/example.html')
