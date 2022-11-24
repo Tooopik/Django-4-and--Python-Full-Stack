a9f38ccdb2d5f9614076d9b4c1477d4ee912f5bc
Adrian Wójcik <wojcik.adrian@protonmail.com>
2022-11-24 19:47:18 +0100

    First variables and filters

--- Django/my_site/my_app/views.py
+++ Django/my_site/my_app/views.py
@@ -6,3 +6,13 @@ from django.shortcuts import render
 def example_view(request):
     # my-app/templates/my_app/example.html
     return render(request, 'my_app/example.html')
+
+
+def variable_view(request):
+    my_var = {
+        'first_name': 'Rosalind',
+        'last_name': 'Franklin',
+        'some_list': [1, 2, 3, 4, 5],
+        'some_dict': {'inside_key': 'inside_value'}
+    }
+    return render(request, 'my_app/variable.html', context=my_var)
